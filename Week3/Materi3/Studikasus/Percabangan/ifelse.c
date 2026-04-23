a4949e4eece8d09f592fb63f6b3ae963755855ac
Najashka <najariski17@gmail.com>
2026-04-23 20:09:32 +0700

    restore Week3

---
+++ Week3/Materi3/Studikasus/Percabangan/ifelse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int nilai = 20;
+
+    if (nilai >= 60){
+        printf("Lulus\n");
+    } else {
+        printf("Tidak lulus\n");
+    }
+
+    return 0;
+}
